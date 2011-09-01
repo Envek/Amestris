1a79032df63c79b1323992cf609422ca94b2a060
Novikov Andrey <envek@envek.name>
2011-09-02 00:17:50 +1000

    Added functions to jQuery for class manipulation in SVG

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// Определим свои функции добавления/удаления класса, так как те, что в jQuery не работают для SVG
+jQuery.fn.myAddClass = function (classTitle) {
+  return this.each(function() {
+    var oldClass = jQuery(this).attr("class");
+    oldClass = oldClass ? oldClass : '';
+    jQuery(this).attr("class", (oldClass+" "+classTitle).trim());
+  });
+}
+jQuery.fn.myRemoveClass = function (classTitle) {
+  return this.each(function() {
+      var oldClass = jQuery(this).attr("class");
+      var startpos = oldClass.indexOf(classTitle);
+      var endpos = startpos + classTitle.length;
+      var newClass = oldClass.substring(0, startpos).trim() + " " + oldClass.substring(endpos).trim();
+      if (!newClass.trim())
+        jQuery(this).removeAttr("class");
+      else
+        jQuery(this).attr("class", newClass.trim());
+  });
+}
+
 // Начинаем работу когда страница полностью загружена (включая графику)
 $(window).load(function () {
   // Получаем доступ к SVG DOM
