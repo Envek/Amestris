794eccdf123849ab6002a6b384aee48a68b93537
Novikov Andrey <envek@envek.name>
2011-09-01 23:04:28 +1000

    A hack for WebKit (for correct map sizing)

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ $(window).load(function () {
   var svgobject = document.getElementById('imap'); 
   if ('contentDocument' in svgobject)
     var svgdom = svgobject.contentDocument;
+
+  // Хак для WebKit (чтобы правильно масштабировал нашу карту)
+  var viewBox = svgdom.rootElement.getAttribute("viewBox").split(" ");
+  var aspectRatio = viewBox[2] / viewBox[3];
+  svgobject.height = parseInt(svgobject.offsetWidth / aspectRatio);
 });
