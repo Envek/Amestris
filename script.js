63ea04f27189e8e53687372864e3b42289fe721e
Novikov Andrey <envek@envek.name>
2011-09-01 22:15:59 +1000

    Accessing the SVG DOM from JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+// Начинаем работу когда страница полностью загружена (включая графику)
+$(window).load(function () {
+  // Получаем доступ к SVG DOM
+  var svgobject = document.getElementById('imap'); 
+  if ('contentDocument' in svgobject)
+    var svgdom = svgobject.contentDocument;
+});
